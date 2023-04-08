ce567708eafbc4615839417bd4077c685a33ff66
Osman Pérez <osman2907@gmail.com>
2023-04-08 09:55:15 -0400

    Integración Google Analytics

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGtag from "vue-gtag";
 
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/main.css";
 
-
 const app = createApp(App);
-
 app.use(router);
+
+app.use(VueGtag, {
+  config: { id: "G-3JN6ZSMD47" },
+});
 
 app.mount("#app");
 
-import "bootstrap/dist/js/bootstrap.js"
-
+import "bootstrap/dist/js/bootstrap.js";
