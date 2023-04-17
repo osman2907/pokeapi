b33331a5ee02e78c291fa285e8c2d54258c620c9
Osman Pérez <osman2907@gmail.com>
2023-04-17 14:30:45 -0400

    Mejoras iconos types

--- src/utils/types.js
+++ src/utils/types.js
@@ -4,16 +4,18 @@ import dragon from "@/assets/img/types/dragon.png";
 import electric from "@/assets/img/types/electric.png";
 import fairy from "@/assets/img/types/fairy.png";
 import fire from "@/assets/img/types/fire.png";
+import fighting from "@/assets/img/types/fighting.png";
 import flighting from "@/assets/img/types/flighting.png";
 import ghost from "@/assets/img/types/ghost.png";
 import grass from "@/assets/img/types/grass.png";
 import ground from "@/assets/img/types/ground.png";
 import ice from "@/assets/img/types/ice.png";
+import normal from "@/assets/img/types/normal.png";
 import poison from "@/assets/img/types/poison.png";
 import psychic from "@/assets/img/types/psychic.png";
+import rock from "@/assets/img/types/rock.png";
 import shadow from "@/assets/img/types/shadow.png";
 import steel from "@/assets/img/types/steel.png";
-import stone from "@/assets/img/types/stone.png";
 import unknown from "@/assets/img/types/unknown.png";
 import water from "@/assets/img/types/water.png";
 import flying from "@/assets/img/types/flighting.png";
@@ -24,16 +26,18 @@ export default {
   dragon,
   electric,
   fairy,
+  fighting,
   flighting,
   ghost,
   ground,
   grass,
   ice,
+  normal,
   psychic,
   poison,
+  rock,
   shadow,
   steel,
-  stone,
   unknown,
   fire,
   water,
